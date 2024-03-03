999a59b84835ac8d729c06c54fabcb660b4d919d
Nguyen Tat Binh <nguyentatbinhtb@gmail.com>
2024-03-03 21:11:58 +0700

    Update log_in log_out and exam

--- script/login.js
+++ script/login.js
@@ -1,3 +1,39 @@
+let apiUser = "http://localhost:3000/user";
+
+//login
+const username = document.querySelector(".input-login-username");
+const password = document.querySelector(".input-login-password");
+const bntLogin = document.querySelector(".login__signInButton");
+
+// get user
+const getUser = async () => {
+  const response = await fetch(apiUser);
+  const data = await response.json();
+  return data;
+};
+
+// login
+bntLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (username.value == "" || password.value == "" ) {
+    alert("Hãy điền đầy đủ thông tin");
+  } else {
+    getUser().then((data) => {
+      const user = data.find(
+        (user) =>
+          user.username == username.value && user.password == password.value
+      );
+      if (user) {
+        alert("Đăng nhập thành công");
+        window.location.href = "./../index.html";
+      } else {
+        alert("Sai thông tin đăng nhập");
+      }
+    });
+  }
+});
+
+
 let isLogin = true;
 let isAdmin = true;
 
@@ -6,3 +42,4 @@ function showAdminMenu() {
         document.getElementById('admin-menu').style.display = 'block';
     }
 }
+
